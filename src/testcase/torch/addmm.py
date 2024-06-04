55b4d21cbc86fb3490dad3847104df0e66e08297
pqcqaq <905739777@qq.com>
2024-06-04 22:08:18 +0800

    bug修复

--- src/testcase/torch/addmm.py
+++ src/testcase/torch/addmm.py
@@ -9,9 +9,9 @@ from src.util.decorator import test_api
 class TorchAddmmTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("1.1.3")
     def test_addmm(self):
-        a = torch.randn(2, 3)
-        b = torch.randn(5, 2, 4)
-        c = torch.randn(5, 4, 3)
-        result = torch.addmm(a, b, c, beta = 0.2, alpha=5)
+        input1 = torch.randn(2, 3)
+        input2 = torch.randn(3, 4)
+        input3 = torch.randn(2, 4)
+        result = torch.addmm(input3, input1, input2)
         return result
 
