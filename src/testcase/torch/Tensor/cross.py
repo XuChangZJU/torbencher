60fe7c3eaa406ca45a3822482a0aef6110141c99
tongwentao <twtzs@foxmail.com>
2024-06-28 09:38:33 +0800

    src/testcase/torch/Tensor/cross.py 被叉乘的那个维度的大小必须为3，已修复。

--- src/testcase/torch/Tensor/cross.py
+++ src/testcase/torch/Tensor/cross.py
@@ -1,26 +1,22 @@
 import torch
 import random
 
-
 from src.testcase.TorBencherTestCaseBase import TorBencherTestCaseBase
 from src.util import test_api_version
 from src.util.decorator import test_api
+
 
 @test_api(torch.Tensor.cross)
 class TorchTensorCrossTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("1.1.3")
     def test_cross_correctness(self):
-        dim = 3  # Cross product is defined for 3-dimensional vectors
-        num_of_elements_each_dim = random.randint(3, 5)  # Random number of elements each dimension, must be at least 3 for cross product
-        input_size = [num_of_elements_each_dim for _ in range(dim)] 
-    
+        dim = random.randint(3, 5)
+        num_of_elements_each_dim = 3  # 被叉乘的那个维度的大小必须为3
+        input_size = [num_of_elements_each_dim for _ in range(dim)]
+
         tensor1 = torch.randn(input_size)
         tensor2 = torch.randn(input_size)
-        cross_dim = random.randint(0, dim-1)  # Random dimension along which to compute the cross product
-    
-        result = torch.cross(tensor1, tensor2, cross_dim)
+        cross_dim = random.randint(0, dim - 1)  # Random dimension along which to compute the cross product
+
+        result = tensor1.cross(tensor2, cross_dim)
         return result
-    
-    
-    
-    
