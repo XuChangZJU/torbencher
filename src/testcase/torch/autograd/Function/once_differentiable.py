3bf58f0f32e375a78066b99cd0a7c3d348963b5b
CAI-JUNZE <1090982865@qq.com>
2024-07-12 22:32:07 +0800

    once_differentiable需要一个非处处可微分的函数

--- src/testcase/torch/autograd/Function/once_differentiable.py
+++ src/testcase/torch/autograd/Function/once_differentiable.py
@@ -6,8 +6,25 @@ from src.util import test_api_version
 from src.util.decorator import test_api
 
 
+class CustomClampFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, input):
+        ctx.save_for_backward(input)
+        # Clamp the input between -1 and 1
+        return torch.clamp(input, min=-1, max=1)
 
-@test_api(torch.autograd.function.once_differentiable)
+    @staticmethod
+    @torch.autograd.function.once_differentiable
+    def backward(ctx, grad_output):
+        input, = ctx.saved_tensors
+        # Gradient is 1 for inputs in the interval [-1, 1], 0 otherwise
+        grad_input = grad_output.clone()
+        grad_input[input.ge(1)] = 0
+        grad_input[input.le(-1)] = 0
+        return grad_input
+
+
+@test_api(CustomClampFunction.apply)
 class TorchAutogradFunctionOncedifferentiableTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("1.1.3")
     def test_once_differentiable_correctness(self):
@@ -17,15 +34,8 @@ class TorchAutogradFunctionOncedifferentiableTestCase(TorBencherTestCaseBase):
         input_size = [num_of_elements_each_dim for i in range(dim)]
         x = torch.randn(input_size, requires_grad=True)
     
-        # Define a custom function with once_differentiable
-        @torch.autograd.function.once_differentiable
-        def my_custom_function(input):
-            # Define the forward pass with an operation that is only differentiable once
-            output = input ** 2
-            return output
-    
         # Apply the custom function
-        result = my_custom_function(x)
+        result = CustomClampFunction.apply(x)
     
         # Calculate gradients
         result.sum().backward()
