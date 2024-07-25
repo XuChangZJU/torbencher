e1db0818ba513561d7ebbe9be4bec51d27224f20
Frankie Buttons <62317502+FrankieNButtons@users.noreply.github.com>
2024-07-25 11:15:33 +0800

    Update __init__.py

--- src/testcase/torch/utils/cpp_extension/__init__.py
+++ src/testcase/torch/utils/cpp_extension/__init__.py
@@ -1,9 +1,36 @@
-from .is_ninja_available import TorchUtilsCppextensionIsninjaavailableTestCase
-from .get_compiler_abi_compatibility_and_version import TorchUtilsCppextensionGetcompilerabicompatibilityandversionTestCase
-from .BuildExtension import TorchUtilsCppextensionBuildextensionTestCase
-from .verify_ninja_availability import TorchUtilsCppextensionVerifyninjaavailabilityTestCase
-from .include_paths import TorchUtilsCppextensionIncludepathsTestCase
-from .load import TorchUtilsCppextensionLoadTestCase
-from .CUDAExtension import TorchUtilsCppextensionCudaextensionTestCase
-from .CppExtension import TorchUtilsCppextensionCppextensionTestCase
-from .load_inline import TorchUtilsCppextensionLoadinlineTestCase
+# from .is_ninja_available import TorchUtilsCppextensionIsninjaavailableTestCase
+# from .get_compiler_abi_compatibility_and_version import TorchUtilsCppextensionGetcompilerabicompatibilityandversionTestCase
+# from .BuildExtension import TorchUtilsCppextensionBuildextensionTestCase
+# from .verify_ninja_availability import TorchUtilsCppextensionVerifyninjaavailabilityTestCase
+# from .include_paths import TorchUtilsCppextensionIncludepathsTestCase
+# from .load import TorchUtilsCppextensionLoadTestCase
+# from .CUDAExtension import TorchUtilsCppextensionCudaextensionTestCase
+# from .CppExtension import TorchUtilsCppextensionCppextensionTestCase
+# from .load_inline import TorchUtilsCppextensionLoadinlineTestCase
+
+import os
+import importlib
+import logging
+
+from src.testcase.TorBencherTestCaseBase import TorBencherTestCaseBase
+
+# 设置日志配置
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+current_directory: str = os.path.dirname(os.path.abspath(__file__))
+script_files: list = [f for f in os.listdir(current_directory) if f.endswith('.py') and f != '__init__.py']
+
+for script_file in script_files:
+    module_name: str = script_file[:-3]  # Remove the .py extension
+    try:
+        module = importlib.import_module(f'.{module_name}', package=__package__)
+        # logger.debug(f"Successfully imported module {module_name}")
+    except Exception as e:
+        logger.debug(f"Failed to import module {module_name}: {e}")
+        continue
+
+    for attribute_name in dir(module):
+        attribute = getattr(module, attribute_name)
+        if isinstance(attribute, type) and issubclass(attribute, TorBencherTestCaseBase):
+            globals()[attribute_name] = attribute
