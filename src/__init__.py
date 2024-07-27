51023c90bb09ed54647f3158cccaa2bbb02c89ed
Frankie Buttons <62317502+FrankieNButtons@users.noreply.github.com>
2024-07-27 18:34:47 +0800

    Update __init__.py
    Yeah

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 from .torbencher import torbencher
 from .TorchWrapper import TorchWrapper
 from .util.bencherDebugger import bencherDebugger
-from singletester import SingleTester;
+from .singletester import SingleTester;
