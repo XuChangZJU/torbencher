7a9d5096e56e83c3a275195abb3c06ea0b304f90
yc808 <350556745@qq.com>
2024-07-29 11:03:11 +0800

    俩改小点scale

--- src/testcase/torch/combinations.py
+++ src/testcase/torch/combinations.py
@@ -10,7 +10,7 @@ from src.util.decorator import test_api
 class TorchCombinationsTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("1.1.3")
     def test_combinations_correctness(self):
-        num_elements = random.randint(3, 10)  # Random number of elements in the 1D tensor
+        num_elements = random.randint(3, 5)  # Random number of elements in the 1D tensor
         tensor_data = torch.randn(num_elements)  # Generate 1D tensor with random values
         r = random.randint(1, num_elements)  # Random r for combination length
         # Get combinations without replacement
--- src/testcase/torch/compile.py
+++ src/testcase/torch/compile.py
@@ -10,8 +10,8 @@ from src.util.decorator import test_api
 class TorchCompileTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("1.1.3")
     def test_compile_correctness(self):
-        dim = random.randint(1, 4)  # Random dimension for the tensor
-        num_of_elements_each_dim = random.randint(1, 5)  # Random number of elements in each dimension
+        dim = random.randint(1, 3)  # Random dimension for the tensor
+        num_of_elements_each_dim = random.randint(1, 3)  # Random number of elements in each dimension
         input_size = [num_of_elements_each_dim for _ in range(dim)]  # Create the tensor shape
 
         def model_function(x):
