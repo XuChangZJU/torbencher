4dcbe81ed039be98ba7eb136c8de820630933de7
Frankie Buttons <62317502+FrankieNButtons@users.noreply.github.com>
2024-07-29 12:30:14 +0800

    Update Linear.py
    A fomulated nn testcase

--- src/testcase/torch/nn/Linear.py
+++ src/testcase/torch/nn/Linear.py
@@ -16,6 +16,10 @@ class TorchNnLinearTestCase(TorBencherTestCaseBase):
         # Instantiate a Linear layer
         linear_layer = torch.nn.Linear(in_features, out_features)
 
+        # initialize the parameters manually
+        linear_layer.weight = torch.nn.Parameter(torch.normal(0, 0.01, size=(out_features, in_features)));
+        linear_layer.bias = torch.nn.Parameter(torch.normal(0, 0.01, size=(out_features,)));
+
         # Randomly generate input tensor
         batch_size = random.randint(1, 10)  # Random batch size between 1 and 10
         input_tensor = torch.randn(batch_size, in_features)
