d2b205767ed70a9e35cafce91bf2d9826d5a4e1f
forrestfug <forrestfug@outlook.com>
2024-08-01 12:00:25 +0800

    mistake

--- debugger_example.py
+++ debugger_example.py
@@ -1,7 +1,40 @@
 from src import bencherDebugger
 
 modules = [
+    "torch.nn.functional",
+    "torch.optim",
+    "torch.special",
+    # "torch.random", # no testcases
+    # "torch.utils.cpp_extension", # skip
+    "torch.utils.data",
+    # "torch.xpu", # skip
+    # "torch.mps", # skip
+    "torch.jit",
+    "torch.utils",
+    "torch.distributions",
+    "torch.autograd",
+    "torch",
+    # "torch.onnx", # no testcases
+    # "torch.cuda", # skip
+    "torch.linalg",
+    # "torch.amp", # no testcases
+    "torch.nn",
+    "torch.utils.mobile_optimizer",
+    # "torch.distributed", # no testcases
+    "torch.utils.checkpoint",
+    "torch.Tensor",
     "torch.export",
+    "torch.profiler",
+    "torch.backends",
+    # "torch.fx", # skip
+    "torch.cpu",
+    # "torch.hub", # skip
+    "torch.testing",
+    # "torch.masked", # skip
+    # "torch.utils.tensorboard", # skip
+    "torch.nn.init",
+    "torch.fft",
+    "torch.autograd"
 ]
 
 debugger = bencherDebugger(
@@ -10,7 +43,7 @@ debugger = bencherDebugger(
         "devices": ["cpu"],
         "test_modules": modules,
         "format": "json",
-        "num_epoches": 1,
+        "num_epoches": 5,
         "including_success": False
     }
 )
