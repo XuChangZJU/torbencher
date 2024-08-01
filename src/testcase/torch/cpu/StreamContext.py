f7bd77f5d000d30f97d436110dfac3c6226b4d3a
tongwentao <twtzs@foxmail.com>
2024-08-01 13:44:56 +0800

    src/testcase/torch/cpu/StreamContext.py 设备写成cuda了，已修复。

--- src/testcase/torch/cpu/StreamContext.py
+++ src/testcase/torch/cpu/StreamContext.py
@@ -11,17 +11,16 @@ from src.util.decorator import test_api
 @test_api(torch.cpu.StreamContext)
 class TorchCpuStreamcontextTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("1.1.3")
-    @unittest.skipIf(not torch.cuda.is_available(), "CUDA is not available")
+    @unittest.skipIf(not torch.cpu.is_available(), "CUDA is not available")
     def test_stream_context_correctness(self):
         # Randomly select a stream priority
         priority = random.randint(-10, 0)  # Random priority between -10 and 0 (valid range for priorities)
-        stream = torch.cuda.Stream(device='cuda', priority=priority)
+        stream = torch.cpu.Stream(priority=priority)
 
         # Create a tensor and perform an operation within the stream context
         tensor_size = [random.randint(1, 5) for _ in range(random.randint(1, 4))]  # Random tensor size
-        tensor = torch.randn(tensor_size, device='cuda')
+        tensor = torch.randn(tensor_size)
 
-        with torch.cuda.stream(stream):
+        with torch.cpu.StreamContext(stream):
             result = tensor * 2  # Example operation within the stream context
-
         return result
