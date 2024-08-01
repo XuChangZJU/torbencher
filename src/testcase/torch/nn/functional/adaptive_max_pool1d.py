988646b93b52678a6aade41afea2323a4f577401
tongwentao <twtzs@foxmail.com>
2024-08-01 13:55:21 +0800

    src/testcase/torch/nn/functional/adaptive_max_pool1d.py 维度只能为2或3，已修复。

--- src/testcase/torch/nn/functional/adaptive_max_pool1d.py
+++ src/testcase/torch/nn/functional/adaptive_max_pool1d.py
@@ -11,9 +11,9 @@ class TorchNnFunctionalAdaptivemaxpool1dTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("1.1.3")
     def test_adaptive_max_pool1d_correctness(self):
         # Random input size
-        dim = random.randint(1, 4)
+        dim = random.randint(2, 3)
         num_of_elements_each_dim = random.randint(1, 5)
-        input_size = [num_of_elements_each_dim for i in range(dim)]
+        input_size = [num_of_elements_each_dim for _ in range(dim)]
         input_size[1] = random.randint(1, 10)  # The length of the signal
 
         # Random input tensor
