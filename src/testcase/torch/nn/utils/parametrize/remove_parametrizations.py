ae65dfd6ea8a0fe79f93ef5387e242f544cc6351
tongwentao <twtzs@foxmail.com>
2024-08-03 14:35:17 +0800

    src/testcase/torch/nn/utils/parametrize/remove_parametrizations.py test_remove_parametrizations_correctness方法加了self参数就可以了。

--- src/testcase/torch/nn/utils/parametrize/remove_parametrizations.py
+++ src/testcase/torch/nn/utils/parametrize/remove_parametrizations.py
@@ -20,7 +20,7 @@ class TestModule(nn.Module):
 @test_api(torch.nn.utils.parametrize.remove_parametrizations)
 class TorchNnUtilsParametrizeRemoveparametrizationsTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("1.1.3")
-    def test_remove_parametrizations_correctness():
+    def test_remove_parametrizations_correctness(self):
         dim = random.randint(1, 4)  # Random dimension for the tensor
         num_of_elements_each_dim = random.randint(1, 5)  # Random number of elements each dimension
         input_size = [num_of_elements_each_dim for _ in range(dim)]
