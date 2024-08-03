9dfe9b9f5daf50b42fa4a5a298c4466c077e11c5
Frankie Buttons <62317502+FrankieNButtons@users.noreply.github.com>
2024-08-03 16:41:41 +0800

    Update torbencherc_example.py

--- torbencherc_example.py
+++ torbencherc_example.py
@@ -18,45 +18,44 @@ config = {
         # "torch",                              # From `TorchCompileTestCase` need checking on non-windows
         # "torch.nn",                           # Can B run through, Plz fixing `Failed`
         # "torch.utils.mobile_optimizer",       # Do not support CUDA.
-        # "torch.utils.checkpoint",             # Well Done
+        "torch.utils.checkpoint",             # Well Done
         # "torch.export",                       # Windows not support Compile, and I can do nothing
         # "torch.profiler",                     # Both support, but can't be judged, no need to test
         # "torch.backends",                     # Empty, needs to be dived into?
-        # "torch.cpu",                          # Well Done
-        # "torch.testing",                      # Well Done
+        "torch.cpu",                          # Well Done
+        "torch.testing",                      # Well Done
         # "torch.nn.init",                      # CPU passed all, CUDA passed half
-        # "torch.fft",                          # Well Done
+        "torch.fft",                          # Well Done
         # "torch.nn.parallel",                  # Need at least 2 GPUs, can't run on Personal Computer
         # "torch.nn.utils",                     # 'TorchNnUtilsClipgradvalueTestCase' stucked, and other `Failed` to fix
         # "torch.nn.utils.parametrize",         # 'TorchNnUtilsParametrizeRemoveparametrizationsTestCase' is a huge problem
         # "torch.nn.utils.prune",               # hard to say, no one can run
         # "torch.nn.utils.parametrizations",    # 'TorchNnUtilsParametrizationsOrthogonalTestCase' is a huge problem
-        # "torch.nn.utils.stateless",           # Only one, Well Done.
+        "torch.nn.utils.stateless",           # Only one, Well Done.
         # "torch.nn.utils.rnn",                 # Needs fixing from 'TorchNnUtilsRnnPacksequenceTestCase'
         # "torch.nn.modules.module",            # Abstruct Object, Hard to fix
         # "torch.nn.modules.lazy",              # Well Done, but device can only B on "device", non-cpu
         # "torch.autograd.forward_ad",          # Well Done
         # "torch.autograd.gradcheck",           # Empty, needs to be dived into?
         # "torch.autograd.graph",               # Only `TorchAutogradGraphRegistermultigradhookTestCase` needs fixing****
-        # "torch.autograd.torch",               # No such module???
         # "torch.autograd.Function",            # Well Done on CPU, None Done on CUDA*****
-        # "torch.autograd.profiler_util",       # Well Done
-        # "torch.autograd.grad_mode",           # Well Done
+        "torch.autograd.profiler_util",       # Well Done
+        "torch.autograd.grad_mode",           # Well Done
         # "torch.autograd.profiler",            # From 'TorchAutogradProfilerLoadnvprofTestCase' need to be fixed
-        # "torch.autograd.functional",          # Well Done
+        "torch.autograd.functional",          # Well Done
         # "torch.autograd.graph.Node",          # From 'TorchAutogradGraphNodeRegisterprehookTestCase' need to be fixed
         # "torch.autograd.Function.FunctionCtx",# Well Done on CPU, Not Done on CUDA at least `TorchAutogradFunctionFunctionctxMarknondifferentiableTestCase`
         # "torch.autograd.profiler.profile",    # Can B run through, Plz fixing `Failed`
         # "torch.backends.opt_einsum",          # Empty, needs to be dived into?
         # "torch.backends.mha",                 # Empty, needs to be dived into?
         # "torch.backends.nnpack",              # Empty, needs to be dived into?
-        # "torch.backends.mps",                 # Well Done
-        # "torch.backends.cpu",                 # Well Done
-        # "torch.backends.cuda",                # Well Done
-        # "torch.backends.mkl",                 # Well Done
-        # "torch.backends.mkldnn",              # Well Done
-        # "torch.backends.openmp",              # Well Done
-        # "torch.backends.cudnn",               # Well Done
+        "torch.backends.mps",                 # Well Done
+        "torch.backends.cpu",                 # Well Done
+        "torch.backends.cuda",                # Well Done
+        "torch.backends.mkl",                 # Well Done
+        "torch.backends.mkldnn",              # Well Done
+        "torch.backends.openmp",              # Well Done
+        "torch.backends.cudnn",               # Well Done
         #**************************************************
         # Below are all empty, needs to be dived into?
 
