7bbacba845be66fd3eec6eeb894c78b7fa8f701f
yc808 <350556745@qq.com>
2024-08-04 13:28:22 +0800

    pack_sequence to cpu

--- src/testcase/torch/nn/utils/rnn/unpack_sequence.py
+++ src/testcase/torch/nn/utils/rnn/unpack_sequence.py
@@ -18,7 +18,10 @@ class TorchNnUtilsRnnUnpacksequenceTestCase(TorBencherTestCaseBase):
         sequences = [torch.randn(random.randint(1, 5)) for _ in range(num_sequences)]
 
         # Pack the sequences
-        packed_sequences = pack_sequence(sequences)
+        # packed_sequences = pack_sequence(sequences)
+
+        lengths = torch.as_tensor([v.size(0) for v in sequences])
+        packed_sequences = torch.nn.utils.rnn.pack_padded_sequence(torch.nn.utils.rnn.pad_sequence(sequences), lengths.cpu())
 
         # Unpack the sequences
         unpacked_sequences = unpack_sequence(packed_sequences)
