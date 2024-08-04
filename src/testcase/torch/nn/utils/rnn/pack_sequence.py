09fbfc1f70f3690ea3fe5b69e81f32aaa6eeddac
yc808 <350556745@qq.com>
2024-08-04 13:07:35 +0800

    torch.nn.utils.rnn.pack_sequence 手动调用和to cpu

--- src/testcase/torch/nn/utils/rnn/pack_sequence.py
+++ src/testcase/torch/nn/utils/rnn/pack_sequence.py
@@ -19,10 +19,16 @@ class TorchNnUtilsRnnPacksequenceTestCase(TorBencherTestCaseBase):
         # Sort lengths in decreasing order
         lengths.sort(reverse=True)
 
+        # lengths = lengths.cpu()
+
         # Generate random sequences based on the lengths
         sequences = [torch.randn(length, random.randint(1, 3)) for length in lengths]
-
+        # for sequence in sequences:
+        #     sequence = sequence.to(torch.device("cpu"))
         # Pack the sequences
-        packed_sequence = torch.nn.utils.rnn.pack_sequence(sequences)
+        # packed_sequence = torch.nn.utils.rnn.pack_sequence(sequences)
 
-        return packed_sequence
+        lengths = torch.as_tensor([v.size(0) for v in sequences])
+        return torch.nn.utils.rnn.pack_padded_sequence(torch.nn.utils.rnn.pad_sequence(sequences), lengths.cpu())
+
+        # return packed_sequence
