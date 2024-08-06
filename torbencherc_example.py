b497bcaf31b8af09505915bcf5e9d9264ba9f804
Frankie Buttons <62317502+FrankieNButtons@users.noreply.github.com>
2024-08-06 18:17:40 +0800

    Update torbencherc_example.py
    释放怪物

--- torbencherc_example.py
+++ torbencherc_example.py
@@ -15,42 +15,42 @@ config = {
         "torch.nn",                             # Can B run through, Plz fixing `Failed`
         "torch.nn.functional",                  # Can B run through, Plz fixing `Failed`
         "torch.special",                        # Can B run through, Plz fixing `Failed`
-        # "torch.autograd",                     # From `TorchAutogradFunctionNestediofunctionTestCase` need checking on cuda
-        # "torch.utils.checkpoint",             # Well Done
-        # "torch.export",                       # Windows not support Compile, and I can do nothing
-        # "torch.profiler",                     # Both support, but can't be judged, no need to test
-        # "torch.profiler.itt",                 # Well Done
+        "torch.autograd",                     # From `TorchAutogradFunctionNestediofunctionTestCase` need checking on cuda
+        "torch.utils.checkpoint",             # Well Done
+        "torch.export",                       # Windows not support Compile, and I can do nothing
+        "torch.profiler",                     # Both support, but can't be judged, no need to test
+        "torch.profiler.itt",                 # Well Done
         "torch.testing",                        # Well Done
-        # "torch.nn.init",                      # CPU passed all, CUDA passed half
+        "torch.nn.init",                      # CPU passed all, CUDA passed half
         "torch.fft",                            # Well Done
-        # "torch.nn.parallel",                  # Need at least 2 GPUs, can't run on Personal Computer
-        # "torch.nn.utils",                     # 'TorchNnUtilsClipgradvalueTestCase' stucked, and other `Failed` to fix
-        # "torch.nn.utils.parametrize",         # 'TorchNnUtilsParametrizeRemoveparametrizationsTestCase' is a huge problem
-        # "torch.nn.utils.prune",               # hard to say, no one can run
-        # "torch.nn.utils.parametrizations",    # 'TorchNnUtilsParametrizationsOrthogonalTestCase' is a huge problem
+        "torch.nn.parallel",                  # Need at least 2 GPUs, can't run on Personal Computer
+        "torch.nn.utils",                     # 'TorchNnUtilsClipgradvalueTestCase' stucked, and other `Failed` to fix
+        "torch.nn.utils.parametrize",         # 'TorchNnUtilsParametrizeRemoveparametrizationsTestCase' is a huge problem
+        "torch.nn.utils.prune",               # hard to say, no one can run
+        "torch.nn.utils.parametrizations",    # 'TorchNnUtilsParametrizationsOrthogonalTestCase' is a huge problem
         "torch.nn.utils.stateless",             # Only one, Well Done.
-        # "torch.nn.utils.rnn",                 # Needs fixing from 'TorchNnUtilsRnnPacksequenceTestCase'
-        # "torch.nn.modules.module",            # Abstruct Object, Hard to fix
-        # "torch.nn.modules.lazy",              # Well Done, but device can only B on "device", non-cpu
-        # "torch.autograd.forward_ad",          # Well Done
-        # "torch.autograd.gradcheck",           # Empty, needs to be dived into?
-        # "torch.autograd.graph",               # Only `TorchAutogradGraphRegistermultigradhookTestCase` needs fixing****
-        # "torch.autograd.Function",            # Well Done on CPU, None Done on CUDA*****
+        "torch.nn.utils.rnn",                 # Needs fixing from 'TorchNnUtilsRnnPacksequenceTestCase'
+        "torch.nn.modules.module",            # Abstruct Object, Hard to fix
+        "torch.nn.modules.lazy",              # Well Done, but device can only B on "device", non-cpu
+        "torch.autograd.forward_ad",          # Well Done
+        "torch.autograd.gradcheck",           # Empty, needs to be dived into?
+        "torch.autograd.graph",               # Only `TorchAutogradGraphRegistermultigradhookTestCase` needs fixing****
+        "torch.autograd.Function",            # Well Done on CPU, None Done on CUDA*****
         "torch.autograd.profiler_util",         # Well Done
         "torch.autograd.grad_mode",             # Well Done
-        # "torch.autograd.profiler",            # From 'TorchAutogradProfilerLoadnvprofTestCase' need to be fixed
+        "torch.autograd.profiler",            # From 'TorchAutogradProfilerLoadnvprofTestCase' need to be fixed
         "torch.autograd.functional",            # Well Done
-        # "torch.autograd.graph.Node",          # From 'TorchAutogradGraphNodeRegisterprehookTestCase' need to be fixed
-        # "torch.autograd.Function.FunctionCtx",# Well Done on CPU, Not Done on CUDA at least `TorchAutogradFunctionFunctionctxMarknondifferentiableTestCase`
-        # "torch.autograd.profiler.profile",    # Can B run through, Plz fixing `Failed`
+        "torch.autograd.graph.Node",          # From 'TorchAutogradGraphNodeRegisterprehookTestCase' need to be fixed
+        "torch.autograd.Function.FunctionCtx",# Well Done on CPU, Not Done on CUDA at least `TorchAutogradFunctionFunctionctxMarknondifferentiableTestCase`
+        "torch.autograd.profiler.profile",    # Can B run through, Plz fixing `Failed`
         # ***************************************************
-        # "torch.optim",                        # No one can run on cuda
-        # "torch.optim.Optimizer",              # No one can run on cuda
-        # "torch.optim.lrscheduler",            # No one can run on cuda
-        # "torch.utils.data",                   # 'TorchUtilsDataGetworkerinfoTestCase' stucked, hard to fix
-        # "torch.utils.data._utils",            # Empty, needs to be dived into?
-        # "torch.utils.data.distributed",       # 'TorchUtilsDataDistributedDistributedsamplerTestCase' can only run on cuda
-        # "torch.utils.data._utils.collate",    # Only one, but Well Done
+        "torch.optim",                        # No one can run on cuda
+        "torch.optim.Optimizer",              # No one can run on cuda
+        "torch.optim.lrscheduler",            # No one can run on cuda
+        "torch.utils.data",                   # 'TorchUtilsDataGetworkerinfoTestCase' stucked, hard to fix
+        "torch.utils.data._utils",            # Empty, needs to be dived into?
+        "torch.utils.data.distributed",       # 'TorchUtilsDataDistributedDistributedsamplerTestCase' can only run on cuda
+        "torch.utils.data._utils.collate",    # Only one, but Well Done
     ],
     "format": "csv",
     "num_epoch": 1,
