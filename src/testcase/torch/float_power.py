a75660e9f080851eef7c3f2303fac7846e1761b7
Nancy5572 <1037922974@qq.com>
2024-08-09 00:06:07 +0800

    update float_power.py

--- src/testcase/torch/float_power.py
+++ src/testcase/torch/float_power.py
@@ -15,7 +15,7 @@ class TorchFloatUpowerTestCase(TorBencherTestCaseBase):
         num_of_elements_each_dim = random.randint(1, 5)  # Random number of elements each dimension
         input_size = [num_of_elements_each_dim for i in range(dim)]
 
-        base = torch.randn(input_size)
+        base = torch.randn(input_size).clamp(min=1e-3)
         exponent = torch.randn(input_size)
         result = torch.float_power(base, exponent)
         return result
