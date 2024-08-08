7a2927a9a6b029cc9addda9ac8c40d1a167acb0f
Frankie Buttons <62317502+FrankieNButtons@users.noreply.github.com>
2024-08-09 00:32:55 +0800

    Update sparse_csr_tensor.py
    remove skip

--- src/testcase/torch/sparse_csr_tensor.py
+++ src/testcase/torch/sparse_csr_tensor.py
@@ -11,7 +11,6 @@ from src.util.decorator import test_api
 @test_api(torch.sparse_csr_tensor)
 class TorchSparseUcsrUtensorTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("2.0.0")
-    @unittest.skip
     def test_sparse_csr_tensor_correctness(self):
         # Random dimension sizes for the sparse matrix
         nrows = random.randint(2, 5)
