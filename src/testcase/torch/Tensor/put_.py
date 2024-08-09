ad0c6da1bd331a65ebf6183078eb94c81071b93f
Frankie Buttons <62317502+FrankieNButtons@users.noreply.github.com>
2024-08-09 13:18:35 +0800

    Update put_.py
    Yeah

--- src/testcase/torch/Tensor/put_.py
+++ src/testcase/torch/Tensor/put_.py
@@ -1,33 +1,24 @@
 import torch
 import random
-
 from src.testcase.TorBencherTestCaseBase import TorBencherTestCaseBase
 from src.util import test_api_version
 from src.util.decorator import test_api
 
 
 @test_api(torch.Tensor.put_)
-class TorchTensorPutTestCase(TorBencherTestCaseBase):
+class TorchTensorPutUTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("1.1.3")
     def test_put__correctness(self):
-        # Define the dimension and size of the input tensor
-        dim = random.randint(1, 4)
-        num_of_elements_each_dim = random.randint(1, 5)
-        input_size = [num_of_elements_each_dim for i in range(dim)]
+        dim_of_tensor = random.randint(1, 3)
+        size_of_dim = random.randint(1, 4)
+        size = [size_of_dim for _ in range(dim_of_tensor)]
 
-        # Create a random tensor 
-        input_tensor = torch.randn(input_size)
+        origin = torch.randn(size)
+        length = torch.numel(origin)
 
-        # Generate random indices to put values
-        # The number of elements in indices should be less than or equal to the number of elements in input_tensor
-        num_indices = random.randint(1, torch.numel(input_tensor))
-        indices = torch.randint(0, torch.numel(input_tensor), (num_indices,), dtype=torch.long)
+        num_to_put = random.randint(1, length)
+        indices_to_put = torch.tensor([random.randint(0, num_to_put - 1) for _ in range(num_to_put)])
+        val_to_put = torch.tensor([random.random() for _ in range(num_to_put)])
 
-        # Create a random tensor of values to put with the same number of elements as indices
-        values_to_put = torch.randn(num_indices)
-
-        # Use put_ to modify the input_tensor in-place
-        input_tensor.put_(indices, values_to_put)
-
-        # Return the modified tensor
-        return input_tensor
+        origin.put_(indices_to_put, val_to_put)
+        return origin
