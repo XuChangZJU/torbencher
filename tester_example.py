25c3b30ecec5dbbd68a1c287136839a50bc33936
Frankie Buttons <62317502+FrankieNButtons@users.noreply.github.com>
2024-08-09 17:09:07 +0800

    Update tester_example.py

--- tester_example.py
+++ tester_example.py
@@ -5,3 +5,7 @@ tester = SingleTester()
 from src.testcase.torch.addmm import TorchAddmmTestCase
 
 tester.run(TorchAddmmTestCase, device="cuda", seed=123)
+
+from src.testcase.torch.quantized_batch_norm import TorchQuantizedUbatchUnormTestCase
+
+tester.run(TorchQuantizedUbatchUnormTestCase, device="cuda", seed=123)
