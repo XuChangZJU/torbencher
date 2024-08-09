6dac2f584846cb9b61daf8915541ff762ca502d6
Nancy5572 <1037922974@qq.com>
2024-08-09 22:31:49 +0800

    update log.py

--- src/testcase/torch/Tensor/log.py
+++ src/testcase/torch/Tensor/log.py
@@ -17,6 +17,6 @@ class TorchTensorLogTestCase(TorBencherTestCaseBase):
         dim = random.randint(1, 4)  # Random dimension for the tensor
         num_of_elements_each_dim = random.randint(1, 5)  # Random number of elements each dimension
         input_size = [num_of_elements_each_dim for i in range(dim)]
-        input_tensor = torch.randn(input_size)  # Random tensor
+        input_tensor = torch.abs(torch.randn(input_size)) + 1e-5  # Random tensor
         result = input_tensor.log()
         return result
