856ee57867dd3a50cc8596635f6c2092e717d32a
Nancy5572 <1037922974@qq.com>
2024-08-09 10:40:53 +0800

    update logdet.py

--- src/testcase/torch/logdet.py
+++ src/testcase/torch/logdet.py
@@ -13,10 +13,14 @@ class TorchLogdetTestCase(TorBencherTestCaseBase):
     def test_logdet_correctness(self):
         # Generate a random square matrix
         dim = random.randint(1, 10)  # Random dimension for the matrix
-        input_size = [dim, dim]
+        # input_size = [dim, dim]
+        #
+        # # Generate a random tensor of the specified size
+        # input_tensor = torch.randn(input_size)
 
-        # Generate a random tensor of the specified size
-        input_tensor = torch.randn(input_size)
+        # 生成正定矩阵
+        A = torch.randn((dim, dim))
+        input_tensor = torch.mm(A, A.t())
 
         # Calculate the log determinant using torch.logdet
         result = torch.logdet(input_tensor)
