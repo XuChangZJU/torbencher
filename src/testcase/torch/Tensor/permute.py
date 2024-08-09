ef4b5a1d6f96b67317216725b984ad5f4abe9189
Nancy5572 <1037922974@qq.com>
2024-08-09 23:21:08 +0800

    update permute.py

--- src/testcase/torch/Tensor/permute.py
+++ src/testcase/torch/Tensor/permute.py
@@ -6,7 +6,8 @@ from src.testcase.TorBencherTestCaseBase import TorBencherTestCaseBase
 from src.util import test_api_version
 from src.util.decorator import test_api
 
-
+def shuffle(lst):
+    return sorted(lst, key=lambda x: random.random())
 @test_api(torch.Tensor.permute)
 class TorchTensorPermuteTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("2.0.0")
@@ -20,7 +21,10 @@ class TorchTensorPermuteTestCase(TorBencherTestCaseBase):
         # Generate a random tensor with the specified input size
         tensor = torch.randn(input_size)
         # Generate a random permutation of the dimensions
-        dims = torch.randperm(dim).tolist()  # Generate a valid permutation of dimensions
+        # dims = torch.randperm(dim).tolist()  # Generate a valid permutation of dimensions
         # Permute the dimensions of the tensor
+        dims = list(range(dim))
+        shuffle(dims)  # Generate valid dims by shuffling
+        dims = tuple(dims)
         result = tensor.permute(dims)
         return result
