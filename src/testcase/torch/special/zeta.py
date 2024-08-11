ab2f19fbe149268d3a9d3e9f148737a397940fd5
tongwentao <twtzs@foxmail.com>
2024-08-11 12:52:19 +0800

    src/testcase/torch/special/zeta.py 修改x和q的取值范围，确保其收敛。

--- src/testcase/torch/special/zeta.py
+++ src/testcase/torch/special/zeta.py
@@ -14,11 +14,13 @@ class TorchSpecialZetaTestCase(TorBencherTestCaseBase):
         # Define random dimensions for the input tensors
         dim = random.randint(1, 4)
         num_of_elements_each_dim = random.randint(1, 5)
-        input_size = [num_of_elements_each_dim for i in range(dim)]
+        input_size = [num_of_elements_each_dim for _ in range(dim)]
 
         # Generate random input tensors x and q
         x = torch.randn(input_size)
+        x = torch.abs(x) + 1.1
         q = torch.randn(input_size)
+        q = torch.abs(q) + 1
 
         # Calculate the Hurwitz zeta function
         result = torch.special.zeta(x, q)
