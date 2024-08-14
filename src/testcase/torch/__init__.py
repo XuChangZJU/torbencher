92a642e002a4467932e9bfccc9b8bc793379abb2
forrestfug <forrestfug@outlook.com>
2024-08-14 14:17:22 +0800

    fixup

--- src/testcase/torch/__init__.py
+++ src/testcase/torch/__init__.py
@@ -1,33 +1,3 @@
-# from .broadcast_shapes import TorchBroadcastshapesTestCase
-# from .stack import TorchStackTestCase
-# from .normal import TorchNormalTestCase
-# from .lobpcg import TorchLobpcgTestCase
-# from .set_num_interop_threads import TorchSetnuminteropthreadsTestCase
-# from .export import TorchExportTestCase
-# from .set_default_device import TorchSetdefaultdeviceTestCase
-# from ._assert import TorchAssertTestCase
-# from .tensor_split import TorchTensorsplitTestCase
-# from .sym_max import TorchSymmaxTestCase
-# from .quantize_per_channel import TorchQuantizeperchannelTestCase
-# from .broadcast_to import TorchBroadcasttoTestCase
-# from .sparse_csc_tensor import TorchSparsecsctensorTestCase
-# from .vstack import TorchVstackTestCase
-# from .slice_scatter import TorchSlicescatterTestCase
-# from .Generator import TorchGeneratorTestCase
-# from .istft import TorchIstftTestCase
-# from .column_stack import TorchColumnstackTestCase
-# from .set_flush_denormal import TorchSetflushdenormalTestCase
-# from .repeat_interleave import TorchRepeatinterleaveTestCase
-# from .sym_min import TorchSymminTestCase
-# from .set_rng_state import TorchSetrngstateTestCase
-# from .frombuffer import TorchFrombufferTestCase
-# from .Gradient import (TorchGradientTestCase)
-# from .get_float32_matmul_precision import TorchGetfloat32matmulprecisionTestCase
-# from .dsplit import TorchDsplitTestCase
-# from .index_reduce import TorchIndexreduceTestCase
-# from .reshape import TorchReshapeTestCase
-# from .sym_ite import TorchSymiteTestCase
-
 import importlib
 import logging
 import os
