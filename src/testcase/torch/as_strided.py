198d77d7c40d6c1c32077b7181206df53237b24c
yc808 <350556745@qq.com>
2024-08-17 07:06:47 +0800

    fix

--- src/testcase/torch/as_strided.py
+++ src/testcase/torch/as_strided.py
@@ -11,14 +11,5 @@ from src.util.decorator import test_api
 class TorchAsUstridedTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("2.0.0")
     def test_as_strided_correctness(self):
-        dim = random.randint(1, 4)  # Random dimension for the tensor
-        num_of_elements_each_dim = [random.randint(2, 5) for _ in
-                                    range(dim)]  # Ensure each dimension has at least 2 elements
-
-        tensor = torch.randn(num_of_elements_each_dim)
-
-        view_size = tuple(random.randint(1, num_of_elements_each_dim[i]) for i in range(dim))  # Random view size
-        view_stride = tuple(random.randint(1, num_of_elements_each_dim[i]) for i in range(dim))  # Random stride
-
-        result = torch.as_strided(tensor, view_size, view_stride)
-        return result
+        x = torch.randn(3, 3)
+        return torch.as_strided(x, (2, 2), (1, 2)), torch.as_strided(x, (2, 2), (1, 2), 1)
