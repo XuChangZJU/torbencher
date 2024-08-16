84d14cc734b7c0ae1bc2fb5b6c054cb996719d90
yc808 <350556745@qq.com>
2024-08-17 07:29:52 +0800

    use example

--- src/testcase/torch/nanmean.py
+++ src/testcase/torch/nanmean.py
@@ -11,14 +11,5 @@ from src.util.decorator import test_api
 class TorchNanmeanTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("2.0.0")
     def test_nanmean_correctness(self):
-        dim = random.randint(0, 3)  # Random dimension for the tensors
-        num_of_elements_each_dim = random.randint(1, 5)  # Random number of elements each dimension
-        input_size = [num_of_elements_each_dim for i in range(dim + 1)]  # Generate input_size
-
-        # Generate random tensor with nan values
-        input_tensor = torch.randn(input_size)
-        mask = torch.rand(input_size) < 0.2  # Randomly set 20% elements to NaN
-        input_tensor[mask] = float('nan')
-
-        result = torch.nanmean(input_tensor, dim)
-        return result
+        x = torch.tensor([[torch.nan, 1, 2], [1, 2, 3]])
+        return x.nanmean(), x.nanmean(dim=0), torch.tensor([torch.nan]).nanmean()
