a3bdf67f33fd353c33d8b84c274dfc2e96d9842f
tongwentao <twtzs@foxmail.com>
2024-08-17 19:13:54 +0800

    src/testcase/torch/nn/Transformer.py 去除具有内部随机性的dropout

--- src/testcase/torch/nn/Transformer.py
+++ src/testcase/torch/nn/Transformer.py
@@ -7,18 +7,18 @@ from src.util import test_api_version
 from src.util.decorator import test_api
 import unittest
 
+
 @test_api(torch.nn.Transformer)
 class TorchNnTransformerTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("2.0.0")
-    @unittest.skip
     def test_transformer_correctness(self):
+        # torch.manual_seed(0) #手动设置随机种子可以通过，但是不手动设置就通过不了，应该是框架没有处理好，先不加，等框架处理
         # Randomly generate parameters for the Transformer model
         d_model = 512  # Number of expected features in the encoder/decoder inputs
         nhead = 8  # Number of heads in the multiheadattention models
         num_encoder_layers = random.randint(1, 6)  # Number of sub-encoder-layers in the encoder
         num_decoder_layers = random.randint(1, 6)  # Number of sub-decoder-layers in the decoder
         dim_feedforward = random.randint(512, 2048)  # Dimension of the feedforward network model
-        dropout = random.uniform(0.0, 0.3)  # Dropout value
 
         # Create the Transformer model with the generated parameters
         transformer_model = torch.nn.Transformer(
@@ -27,7 +27,7 @@ class TorchNnTransformerTestCase(TorBencherTestCaseBase):
             num_encoder_layers=num_encoder_layers,
             num_decoder_layers=num_decoder_layers,
             dim_feedforward=dim_feedforward,
-            dropout=dropout
+            dropout=0.0
         )
 
         # Randomly generate input tensors for the source and target sequences
