df56cd2b0198223b53bd99b9e6300100b929e6f7
Nancy5572 <1037922974@qq.com>
2024-08-17 23:49:39 +0800

    update LazyLinear.py

--- src/testcase/torch/nn/LazyLinear.py
+++ src/testcase/torch/nn/LazyLinear.py
@@ -10,7 +10,7 @@ import unittest
 @test_api(torch.nn.LazyLinear)
 class TorchNnLazylinearTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("2.0.0")
-    @unittest.skip
+    # @unittest.skip
     def test_lazylinear_correctness(self):
         # Randomly generate input size
         dim = random.randint(1, 4)
@@ -27,5 +27,15 @@ class TorchNnLazylinearTestCase(TorBencherTestCaseBase):
         lazy_linear = torch.nn.LazyLinear(out_features)
 
         # Forward pass to initialize weights and bias
+        result = lazy_linear(input_tensor)
+        with torch.no_grad():
+            lazy_linear.weight = torch.nn.Parameter(
+                torch.randn(lazy_linear.weight.shape) * 0.01
+            )
+            if lazy_linear.bias is not None:
+                lazy_linear.bias = torch.nn.Parameter(
+                    torch.randn(lazy_linear.bias.shape) * 0.01
+                )
+
         result = lazy_linear(input_tensor)
         return result
