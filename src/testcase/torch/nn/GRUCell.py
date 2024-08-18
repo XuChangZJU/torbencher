5cf1b2c8536679d5ad36bdbc35ee3d0814d531ae
tongwentao <twtzs@foxmail.com>
2024-08-18 11:44:53 +0800

    src/testcase/torch/nn/GRUCell.py 手动指定权重后通过

--- src/testcase/torch/nn/GRUCell.py
+++ src/testcase/torch/nn/GRUCell.py
@@ -7,10 +7,10 @@ from src.util import test_api_version
 from src.util.decorator import test_api
 import unittest
 
+
 @test_api(torch.nn.GRUCell)
 class TorchNnGrucellTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("2.0.0")
-    @unittest.skip
     def test_GRUCell_correctness(self):
         # Randomly generate input size
         input_size = random.randint(1, 10)
@@ -23,6 +23,14 @@ class TorchNnGrucellTestCase(TorBencherTestCaseBase):
 
         # Create GRUCell module
         gru_cell = torch.nn.GRUCell(input_size, hidden_size)
+        num_chunks = 3  # 从GRUCell的源代码可知，这个是写死的
+        with torch.no_grad():
+            gru_cell.weight_ih = torch.nn.Parameter(
+                torch.randn((num_chunks * gru_cell.hidden_size, gru_cell.input_size)))
+            gru_cell.weight_hh = torch.nn.Parameter(
+                torch.randn((num_chunks * gru_cell.hidden_size, gru_cell.hidden_size)))
+            gru_cell.bias_ih = torch.nn.Parameter(torch.randn(num_chunks * gru_cell.hidden_size))
+            gru_cell.bias_hh = torch.nn.Parameter(torch.randn(num_chunks * gru_cell.hidden_size))
 
         # Create random input tensor
         input_tensor = torch.randn(seq_len, batch_size, input_size)
