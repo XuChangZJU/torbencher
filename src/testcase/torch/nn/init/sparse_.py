f0bca0bab7fa3da16765603ab876d8b2be16732a
yc808 <350556745@qq.com>
2024-08-20 17:11:34 +0800

    skip

--- src/testcase/torch/nn/init/sparse_.py
+++ src/testcase/torch/nn/init/sparse_.py
@@ -1,4 +1,5 @@
 import random
+import unittest
 
 import torch
 
@@ -10,6 +11,7 @@ from src.util.decorator import test_api
 @test_api(torch.nn.init.sparse_)
 class TorchNnInitSparseUTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("2.0.0")
+    @unittest.skip("内部随机")
     def test_sparse_correctness(self):
         # Randomly generate dimensions for a 2D tensor
         rows = random.randint(1, 10)
