892a25a936da42db0edced538a947af790cfe6d4
Frankie Buttons <62317502+FrankieNButtons@users.noreply.github.com>
2024-08-28 13:34:01 +0800

    Update fake_quantize_per_tensor_affine.py

--- src/testcase/torch/fake_quantize_per_tensor_affine.py
+++ src/testcase/torch/fake_quantize_per_tensor_affine.py
@@ -1,7 +1,4 @@
-import random
-
 import torch
-
 from src.testcase.TorBencherTestCaseBase import TorBencherTestCaseBase
 from src.util import test_api_version
 from src.util.decorator import test_api
@@ -12,4 +9,13 @@ class TorchFakeUquantizeUperUtensorUaffineTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("2.0.0")
     def test_fake_quantize_per_tensor_affine_correctness(self):
         x = torch.randn(4)
-        return x, torch.fake_quantize_per_tensor_affine(x, 0.1, 0, 0, 255), torch.fake_quantize_per_tensor_affine(x, torch.tensor(0.1), torch.tensor(0), 0, 255)
+
+        # Ensure scale and zero_point are on the same device as x
+        scale = torch.tensor(0.1, device=x.device)
+        zero_point = torch.tensor(0, device=x.device, dtype=torch.int)
+
+        return (
+            x,
+            torch.fake_quantize_per_tensor_affine(x, 0.1, 0, 0, 255),  # Scalars 0.1 and 0 are automatically on CPU
+            torch.fake_quantize_per_tensor_affine(x, scale, zero_point, 0, 255)
+        )
