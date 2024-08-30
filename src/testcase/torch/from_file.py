2087d6526d162792b07358685fb516283f09e2c7
Frankie Buttons <62317502+FrankieNButtons@users.noreply.github.com>
2024-08-30 16:24:37 +0800

    Update from_file.py
    to("cpu")

--- src/testcase/torch/from_file.py
+++ src/testcase/torch/from_file.py
@@ -12,6 +12,6 @@ class TorchFromUfileTestCase(TorBencherTestCaseBase):
     @test_api_version.larger_than("2.0.0")
     def test_from_file_correctness(self):
         t = torch.randn(2, 5, dtype=torch.float64)
-        t.numpy().tofile('storage.pt')
+        t.to("cpu").numpy().tofile('storage.bin')
         t_mapped = torch.from_file('storage.pt', shared=False, size=10, dtype=torch.float64)
         return t_mapped
